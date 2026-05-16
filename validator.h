968196d59d071eca012c743134a178d16a3ad53b
Hathazi Alexandru-Ioan <192125074+Hazy2006@users.noreply.github.com>
2026-05-16 22:11:30 +0300

    Delete validator.h

--- validator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "domain.h"
-#include "exceptions.h"
-
-class TutorialValidator {
-public:
-    // Validates a Tutorial's fields.
-    static void validate(const Tutorial& t);
-};
